77eba629556f8f6019d59d130c3f94a06c54f497
Parvaneh Hadi <53343634+parvaneh9492@users.noreply.github.com>
2021-09-29 02:24:28 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" >
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="nav">
+      <h1 class="logo">Butterfly Galaxy</h1>
+
+      <div class="menu-links">
+        <ul>
+          <li>Home</li>
+          <li>Galaxy</li>
+          <li>Missions</li>
+          <li>students</li>
+          <li>Science Questions</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div class="search-box">
+        <input class="search-txt" type="text" name="" placeholder="Search...">
+        <a href="#" class="search-btn"></a>
+        <i class="fas fa-search"></i>
+      </div>
+    </div>
+
+    <div class="content">
+
+      <div class="title">Earth</div>
+
+      <button class="more" type="submit"><a href="#">Let's Start!</a></button>
+
+      <div class="desc">
+        <p>
+          Start<span> your journey</span> with <br>
+          diving into universe
+        </p>
+        <p>
+        The physical Universe is defined as all of space and time and their contents. Such contents comprise all of energy in its various forms, including electromagnetic radiation and matter,...
+        </p>
+      </div>
+    </div>
+
+    <div class="earth">
+      <img src="./img/earth1.webp" alt="">
+    </div>
+
+        <div class="sun">
+           <img src="./img/sun1.png" alt="">
+        </div>
+
+    <div class="moon">
+        <img src="./img/moon1.png" alt="">
+    </div>
+
+    <div class="arrows">
+      <button class="prev"><i class="fa fa-chevron-left"></i></button>
+      <button class="next"><i class="fa fa-chevron-right"></i></button>
+    </div>
+
+  </div>
+
+  
+
+
+</body>
+</html>
